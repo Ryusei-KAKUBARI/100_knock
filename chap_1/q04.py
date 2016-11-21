960d350a5cb8a6143254958e0b6cb4dc431f3cbb
Ryusei-KAKUBARI <kakubari.ryusei@gmail.com>
2016-11-21 17:05:17 +0900

    100本ノック　1章 問04.　修正(スライスの条件を訂正)

--- chap_1/q04.py
+++ chap_1/q04.py
@@ -9,7 +9,7 @@ number = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 word_list = sentence.replace(".", "").split()
 for i, word in enumerate(word_list):
     if i+1 in number:
-        periodic_table[i+1] = word[:1:1]
+        periodic_table[i+1] = word[:1]
     else:
-        periodic_table[i+1] = word[:2:1]
+        periodic_table[i+1] = word[:2]
 print(periodic_table)
